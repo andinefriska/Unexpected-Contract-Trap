56e13abaa173381756e0c2f55113cee55fe0f477
andinefriska <andinefriska@gmail.com>
2025-09-27 19:03:44 +0700

    Update UnexpectedContractTrap.sol

--- src/UnexpectedContractTrap.sol
+++ src/UnexpectedContractTrap.sol
@@ -4,29 +4,32 @@ pragma solidity 0.8.20;
 import {ITrap} from "drosera-contracts/interfaces/ITrap.sol";
 
 contract UnexpectedContractTrap is ITrap {
-    constructor() {}
+    address constant YOUR_WALLET = 0x1111111111111111111111111111111111111111;
+
+    // Hardcode whitelist
+    address constant WHITELIST1 = 0x0000000000000000000000000000000000000001;
+    address constant WHITELIST2 = 0x0000000000000000000000000000000000000002;
+    address constant WHITELIST3 = 0x0000000000000000000000000000000000000003;
 
     function collect() external view override returns (bytes memory) {
-        return abi.encode(msg.sender);
+        return abi.encode(YOUR_WALLET);
     }
 
     function shouldRespond(bytes[] calldata data) external pure override returns (bool, bytes memory) {
-        if (data.length < 2) {
-            return (false, abi.encode(address(0), "Not enough history"));
-        }
+        if (data.length == 0) return (false, "");
 
-        address newContract = abi.decode(data[data.length - 1], (address));
-        address oldContract = abi.decode(data[data.length - 2], (address));
+        address wallet = abi.decode(data[0], (address));
 
-        bool hasUnexpected = (newContract != oldContract);
-
-        bytes memory responseData = abi.encode(
-            newContract,
-            hasUnexpected
-                ? "ALERT: Unexpected contract interaction detected!"
-                : "No change, interaction consistent"
+        bool isWhitelisted = (
+            wallet == WHITELIST1 ||
+            wallet == WHITELIST2 ||
+            wallet == WHITELIST3
         );
 
-        return (hasUnexpected, responseData);
+        if (!isWhitelisted) {
+            return (true, abi.encode(wallet, block.number));
+        }
+
+        return (false, "");
     }
 }
